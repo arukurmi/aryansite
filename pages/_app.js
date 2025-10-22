d46be680eb994f444a3daf446a2d961d0f260c7b
Aryansh Kurmi <arukurmi22@gmail.com>
2025-10-23 02:11:11 +0530

    feature-dark-theme add contexts/ThemeContext.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,5 @@
 import '../styles/globals.css'
-import { ThemeProvider } from '../contexts/ThemeContext'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  )
+  return <Component {...pageProps} />
 }
